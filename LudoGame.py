60bbab0d73edc839449e1fe24f024b240f6948c6
LoganC <alclary@posteo.net>
2022-08-07 19:54:38 -0500

    docstrings

--- LudoGame.py
+++ LudoGame.py
@@ -25,66 +25,142 @@ class Player:
         self._stacked = False # T or F if tokens stacked
 
     def get_completed(self):
-        # returns True or False if the player has finished or not finished the game
+        """returns True or False if the player has finished or not finished the game
+
+        Returns:
+            bool: True if player has finished game, else false
+        """
         return True if self._state == 'FINISHED' else False
 
     def set_token(self, token, current_pos):
+        """setter method to set value of given token (p or q)
+
+        Args:
+            token (str): token to be set ('p' or 'q')
+            current_pos (int): updated position to be set (-1 to 57 valid range)  # TODO confirm correct
+        """
         if token == 'p':
             self._position['p'][0] = current_pos
         elif token == 'q':
             self._position['q'][0] = current_pos
 
     def get_token(self, token):
+        """getter method to obtain given token's current position
+
+        Args:
+            token (str): token to retrieve value of ('p' or 'q')
+
+        Returns:
+            int: current position of given token
+        """
         if token == 'p':
             return self.get_token_p_step_count()
         elif token == 'q':
             return self.get_token_q_step_count()
 
     def is_homerow(self, token):
+        """return true if given token is currently located in the player's homerow
+
+        Args:
+            token (str): token to retrieve status of ('p' or 'q')
+
+        Returns:
+            bool: True if token is in homerow, else False
+        """
         if token == 'p':
             return self._position['p'][1]
         elif token == 'q':
             return self._position['q'][1]
 
     def set_homerow(self, token, is_homerow):
+        """set boolean value of given token, to indicate if currently in homerow
+
+        Args:
+            token (str): token to set status of ('p' or 'q')
+            is_homerow (bool): True if token is in homerow, else False
+        """
         if token == 'p':
             self._position['p'][1] = is_homerow
         elif token == 'q':
             self._position['q'][1] = is_homerow
 
     def is_stacked(self):
+        """getter method to indicate if tokens for player are currently stacked
+
+        Returns:
+            bool: True if tokens are stacked, else False
+        """
         return self._stacked
 
     def set_stacked(self, bool_val):
+        """setter method to indicated/update if tokens for player are currently stacked
+
+        Args:
+            bool_val (bool): True if tokens are stacked, else False
+        """
         self._stacked = bool_val
 
     def get_token_p_step_count(self):
-        # takes no parameters and returns the total steps the token p has taken on the board (use
-        # steps = -1 for home yard current_pos and steps = 0 for ready to go current_pos) The total step
-        # should not be larger than 57.
+        """returns the total steps the token p has taken on the board (uses steps = -1 for
+            home yard current_pos and steps = 0 for ready to go current_pos)
+
+        Returns:
+            int: total steps the token p has taken
+        """
         return self._position['p'][0]
 
     def get_token_q_step_count(self):
-        # takes no parameters and returns the total steps the token q has taken on the board (use
-        # steps = -1 for home yard current_pos and steps = 0 for ready to go current_pos)
+        """returns the total steps the token q has taken on the board (uses steps = -1 for
+            home yard current_pos and steps = 0 for ready to go current_pos)
+
+        Returns:
+            int: total steps the token q has taken
+        """
         return self._position['q'][0]
 
     def get_start(self):
+        """getter method to return relative board starting place of player
+
+        Returns:
+            int: relative board starting place of player
+        """
         return self._start
 
     def get_end(self):
+        """getter method to return relative board end place of player
+
+        Returns:
+            int: relative board end place of player
+        """
         return self._end
 
     def get_state(self):
+        """getter method to return current state of player (e.g. None, 'PLAYING', or 'FINISHED')
+
+        Returns:
+            None/string: current state of player
+        """
         return self._state
 
-    def set_state(self, state_update):
-        self._state = state_update
+    def set_state(self, new_state):
+        """setter method to update current state of player (e.g. None, 'PLAYING', or 'FINISHED')
+
+        Args:
+            new_state (None/string): updated state of player
+        """
+        self._state = new_state
 
     def get_space_name(self, total_steps):
-        # takes as a parameter the total steps of the token and returns the name of the space the
-        # token has landed on on the board as a string. It should be able to return the home yard
-        # current_pos (‘H’) and the ready to go current_pos (‘R’) as well.
+        """takes as a parameter the total steps of the token and returns the name of the space the
+            token has landed on on the board as a string. Returns 'H' for homeyard, 'R' for ready-to-go,
+            'F' for finish, and player-letter prefixed digits for homerow.
+
+        Args:
+            total_steps (int): total steps taken by a token for a given player
+
+        Returns:
+            string: letter or number (as string) designation of board space
+        """
         relative_range = range(self._start, self._start + 50)
         homerow = range(self._end + 1, self._end + 6)
         finish = self._end + 7
@@ -114,22 +190,33 @@ class LudoGame:
             }
 
     def get_player_by_position(self, letter):
+        """getter method to obtain Player object from given letter designation for player (e.g. 'A', 'B', 'C', or 'D')
+
+        Args:
+            letter (string): letter designation for player (e.g. A, B, C, or D)
+
+        Returns:
+            Player/string: player object or not found message
+        """
         if letter in self._active_players.keys():
-            return self._active_players[letter]   # Shortcut to player obj via letter (A,B,C,D)
+            return self._active_players[letter]
         else:
             return "Player not found!"
 
     def move_token(self, player, token, steps):
-        # method will take care of one token moving on the board. It will also update the token’s
-        # total steps, and it will take care of kicking out other opponent tokens as needed. The
-        # play_game method will use this method.
+        """handles moving of given token on the board; also updates the token's total steps;
+            and, calls additional methods to handle kicking and stacking, as needed.
 
+        Args:
+            player (Player): Player object
+            token (string): token to be moved (e.g. 'p' or 'q')
+            steps (int): number of steps to be moved
+        """
         current_pos = player.get_token(token)
         start = player.get_start()
         end = player.get_end()
         finish = end + 7
 
-        # 1. Move token (update player)
         if current_pos == -1:
             if steps == 6:
                 player.set_token(token, 0)
@@ -178,11 +265,29 @@ class LudoGame:
         self.check_finished()
 
     def handle_stack(self, initiator, token, new_pos):
+        """method to assist in identifying and handling players stacking their tokens;
+            if players other token is detected occupying the space that given token is
+            moving to (new_pos), then Player object will be updated to indicate tokens
+            are stacked.
+
+        Args:
+            initiator (Player): the player that is initiating the move
+            token (string): token being moved (e.g. 'p' or 'q')
+            new_pos (int): the new position the token will be moved to
+        """
         if initiator.get_token(self.other_token(token)) == new_pos:
             initiator.set_stacked(True)
         return
 
     def handle_kick(self, initiator, new_pos):
+        """method to assist in identifying and handling kicking of opponent back to homeyard;
+            if opponent token is detected occupying the space that given token is moving to
+            (new_pos), then opponent token will be 'kicked' back to their homeyard.
+
+        Args:
+            initiator (Player): the player that is initiating the move
+            new_pos (int): the new position the token will be moved tos
+        """
         for player in self._active_players.values():
             if (player is not initiator and
                     player.get_token('p') == new_pos and
@@ -199,12 +304,24 @@ class LudoGame:
         return
 
     def other_token(self, token):
+        """helper function to quickly indicate opposite of given token
+            i.e. if p, then q; if q, then p
+
+        Args:
+            token (string): given token (e.g. 'p' or 'q')
+
+        Returns:
+            string: opposite token (i.e. if p, then q; if q, then p)
+        """
         if token == 'p':
             return 'q'
         elif token == 'q':
             return 'p'
 
     def check_finished(self):
+        """function to identifying if win conditions have been met by a player and, if so,
+            update Player object's state to 'FINISHED'
+        """
         for player in self._active_players.values():
             end = player.get_end()
             finish = end + 7
@@ -215,16 +332,18 @@ class LudoGame:
                 player.set_state('FINISHED')
 
     def play_game(self, player_list, turns_list):
-        # The players list is the list of positions players choose, like [‘A’, ‘C’] means two players
-        #  will play the game at current_pos A and C. Turns list is a list of tuples with each tuple a
-        #  roll for one player. For example, [('A', 6), ('A', 4), ('C', 5)] means player A rolls 6, then
-        #  rolls 4, and player C rolls 5. This method will create the player list first using the players
-        #  list pass in, and then move the tokens according to the turns list following the priority rule
-        #  and update the tokens current_pos and the player’s game state (whether finished the game or not).
-        #  After all the moving is done in the turns list, the method will return a list of strings representing
-        #  the current spaces of all of the tokens for each player in the list after moving the tokens following
-        #  the rules described above. ‘H’ for home yard, ‘R’ for ready to go current_pos, ‘E’ for finished
-        #  current_pos, and other letters/numbers for the space the token has landed on.
+        """method creates the player list using the players list passed in; then moves tokens according
+            to the given turns_list following the priority rule (set in token_to_move) and update the
+            tokens current_pos and the player's game state.
+
+        Args:
+            player_list (list): list of player letters that will participate in game (e.g. ['A','B','C'])
+            turns_list (list): list of tuples, specifying player letter and dice roll for turn
+                (e.g. [('A':4),('B':3),('A':6),('A':3)])
+
+        Returns:
+            list: a list of strings representing the current spaces of all of the tokens for each player
+        """
 
         self.player_create_validate(player_list)
 
@@ -249,6 +368,16 @@ class LudoGame:
         return current_board
 
     def player_create_validate(self, player_list):
+        """function to validate and create active players for the for the LudoGame object,
+            given the player_list passed from that function
+
+        Args:
+            player_list (list): list of player letters that will participate in game (e.g. ['A','B','C'])
+
+        Raises:
+            DuplicatePlayerError: indicates that same player is specified more than one or already exists
+            PlayerLetterError: indicates that an item in player_list was not a one of the valid letters
+        """
         try:
             for player in player_list:
                 if player.upper() in ['A','B','C','D']:
@@ -266,6 +395,16 @@ class LudoGame:
             print("ERROR:  The same player was specified more than once. Please correct the player list.")
 
     def token_to_move(self, player, dice_roll):
+        """decision algorithm, based on a priority ruleset, to decide which of given player's
+            token should be moved next (e.g. 'p' or 'q').
+
+        Args:
+            player (Player): player whose token will be evaluated and moved
+            dice_roll (int): dice roll, corresponding to number of steps token will move
+
+        Returns:
+            string: token that should be moved accoriding to priority ruleset (e.g. 'p' or 'q')
+        """
         #  decision-making algorithm for a player to choose a certain token to move.
         token_p = player.get_token_p_step_count()
         token_q = player.get_token_q_step_count()
@@ -285,23 +424,15 @@ class LudoGame:
                 return 'p'
             elif token_q == -1:
                 return 'q'
+        # 2. If one token is already in the home square and the step number is exactly what is
+        #   needed to reach the end square, let that token move and finish
         else:
-            # 2. If one token is already in the home square and the step number is exactly what is
-            #   needed to reach the end square, let that token move and finish
             if dice_roll == p_to_finish:
                 return 'p'
             elif dice_roll == q_to_finish:
                 return 'q'
 
-
-            # if (self.get_player_by_position(player.get_space_name(token_p + dice_roll)) is not None and
-            #         token_p != -1):
-            #     return 'p'
-            # elif (self.get_player_by_position(player.get_space_name(token_q + dice_roll)) is not None and
-            #         token_q != -1):
-            #     return 'q'
-            # 3. If one token can move and kick out an opponent token, then move that token
-
+        # 3. If one token can move and kick out an opponent token, then move that token
             for active_player in self._active_players.values():
                 if active_player is player:
                     continue
@@ -319,16 +450,18 @@ class LudoGame:
                     elif move_q_to == opponent_q and not opp_q_homerow:
                         return 'q'
 
-            # 4. Move the token that is further away from the finishing square
+        # 4. Move the token that is further away from the finishing square
             if p_to_finish > q_to_finish or token_q == -1:
                 return 'p'
             else:
                 return 'q'
 
 class PlayerLetterError(Exception):
+    """exception to indicate that an item in player_list was not a one of the valid letters"""
     pass
 
 class DuplicatePlayerError(Exception):
+    """excpetion to indicate that same player is specified more than one or already exists"""
     pass
 
 def main():
@@ -336,10 +469,10 @@ def main():
     turns = [('A', 6), ('A', 4), ('A', 5), ('A', 4), ('B', 6), ('B', 4), ('B', 1), ('B', 2), ('A', 6), ('A', 4), ('A', 6), ('A', 3), ('A', 5), ('A', 1), ('A', 5), ('A', 4)]
     game = LudoGame()
     current_tokens_space = game.play_game(players, turns)
-    # player_A = game.get_player_by_position('A')
-    # print(player_A.get_completed())
-    # print(player_A.get_token_p_step_count())
-    # print(current_tokens_space)
+    player_A = game.get_player_by_position('A')
+    print(player_A.get_completed())
+    print(player_A.get_token_p_step_count())
+    print(current_tokens_space)
     player_B = game.get_player_by_position('B')
     print(player_B.get_space_name(55))
 
